74193f6c7b7f314fd4f56ad1841b764775d3174e
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-11-30 23:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const html = `
+<tr>
+<td>New Cell1</td>
+<td>New Cell2</td>
+</tr>
+`;
+const table = document.querySelector('#sampleTable');
 function insert_Row() {
     //Write your code here
-  
-  
+  table.insertAdjacentHTML('afterbegin' , html);
 }
